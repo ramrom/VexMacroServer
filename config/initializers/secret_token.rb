5b5adf31c66d81ef664f50fa75a2c039c515752a
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2014-09-20 14:19:26 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vexmacroserver::Application.config.secret_key_base = '7a942fa43fc5a0623695d4650c9ce7ac900a52f11845dd0ad859f47a1ad4a2bdec41264985c86d47175982fb060c55022fdeb7684a32898112a5dfe07dfb957b'
